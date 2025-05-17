00900e3cc4a74bb2663d1b7ae588d3364599003e
karannnn_exe <karandeep.singh@tothenew.com>
2025-05-17 06:57:26 +0530

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket    = "my-terraform-gitops-state"
-    key       = "demo/terraform.tfstate"
-    region    = "ap-southeast-2"
-    lock_file = true
+    bucket       = "my-terraform-gitops-state"
+    key          = "demo/terraform.tfstate"
+    region       = "ap-southeast-2"
+    use_lockfile = true
   }
 }
 
